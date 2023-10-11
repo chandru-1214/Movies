ebaa7f3462f3bf35d3255857fd338ea4b86d6f1a
Chandru M <chandrum@Chandrus-MacBook-Air.local>
2023-10-11 15:49:32 +0530

    Updated image display functionality

--- Movies/Helpers/UIUtils.swift
+++ Movies/Helpers/UIUtils.swift
@@ -14,21 +14,49 @@ class UIUtils {
     ///   - imageURl: Denotes image url
     ///   - imageView: Denotes imageview
     static func setImage(_ imageURl: String?, imageView: UIImageView) {
-        imageView.image = UIImage(named: "default_movie_icon")
-        let dispatchQueue = DispatchQueue(label: "upadte.image", attributes: .concurrent)
+        
         if let posterURL = imageURl,
            posterURL != "N/A",
            let posterUrl = URL(string: posterURL) {
+            
+            let dispatchQueue = DispatchQueue(label: "update.image")
+            
             dispatchQueue.async {
-                do {
-                    let posterData = try Data(contentsOf: posterUrl)
-                    DispatchQueue.main.async {
-                        imageView.image = UIImage(data: posterData)
+                let urlSession = URLSession(configuration: .default)
+                let sessionTask = urlSession.downloadTask(with: posterUrl) { data, response, error in
+                    if error != nil {
+                        print("Error: \(String(describing: error))")
+                        setImage(for: imageView)
                     }
-                } catch {
-                    // print("Image Load Error: \(error.localizedDescription)")
+                    
+                    if let httpResponse = response as? HTTPURLResponse,
+                       httpResponse.statusCode == 200,
+                       let data = data {
+                        do {
+                            let posterData = try Data(contentsOf: data)
+                            setImage(UIImage(data: posterData), for: imageView)
+                        } catch {
+                            print("image conversion error: \(error.localizedDescription)")
+                            setImage(for: imageView)
+                        }
+                    } else {
+                        print("data is empty")
+                        setImage(for: imageView)
+                    }
+                    
                 }
+                sessionTask.resume()
             }
+            
+        } else {
+            imageView.image = UIImage(named: "default_movie_icon")
+        }
+    }
+    
+    /// Method to update image in main thread   
+    static func setImage(_ image: UIImage? = UIImage(named: "default_movie_icon"), for imageView: UIImageView) {
+        DispatchQueue.main.async {
+            imageView.image = image
         }
     }
 }
